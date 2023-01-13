7cef5fd90534f461bba615d25c06d9f34994985e
JJ <kongpopsattarach.edu@gmail.com>
2023-01-13 17:00:19 +0700

    update

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,40 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum1 = 0;
+    double sum2 = 0;
+    double sum3 = 1;
+    double sum4 = 0;
+    double max = 0;
+    double min = 99;
+    for(int i = 0;i < N;i++){
+        sum1 = sum1 + A[i];
+    }
+        B[0] = sum1/N;
+    for(int i = 0;i < N;i++){
+        sum2 = sum2 + pow(A[i] - B[0],2);
+    }
+        B[1] = sqrt(sum2/N);
+    for(int i = 0;i < N;i++){
+        sum3 = sum3 * A[i];
+    }
+        B[2] = pow(sum3,1.0/N);
+    for(int i = 0;i < N;i++){
+        sum4 = sum4 + 1/A[i];
+    }
+        B[3] = N/sum4;
+    for(int i = 0;i < N;i++){
+        if (A[i] > max)
+        {
+            max = A[i];
+        }
+        B[4] = max;
+        if (A[i] < min)
+        {
+            min = A[i];
+        }
+        B[5] = min;
+    }
+}
+
